51c58a1fe861a1baa92c0787d0fdbda5b0f1fac6
jonachehilton <jonache.hilton@my.jcu.edu.au>
2019-09-18 13:09:15 +1000

    Add standard methods for movie class

--- movie.py
+++ movie.py
@@ -5,5 +5,19 @@
 
 
 class Movie:
-    """..."""
-    pass
+    def __init__(self, title="", year=0, category="", watched=False):
+        self.title = title
+        self.year = year
+        self.category = category
+        self.watched = watched
+
+    def __str__(self):
+        return "{} ({}) from {}".format(self.title, self.category, self.year)
+
+    def is_watched(self):
+        if self.watched:
+            return "this movie is watched"
+
+
+
+
