22cc4ba1e7e5040db64fc288a8645870e9e349d2
Jonache Hilton <jonache.hilton@my.jcu.edu.au>
2019-10-14 19:53:29 +1000

    Add __init__ method which has a list attribute with some test Movie objects inside

--- main.py
+++ main.py
@@ -15,6 +15,13 @@ from moviecollection import MovieCollection
 
 class MoviesToWatchApp(App):
     """..."""
+    status_text = StringProperty
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.movies = [Movie("Castaway", 1990, "Drama", True),
+                       Movie("Star Wars", 2002, "Action", False),
+                       Movie("King Kong", 2010, "Thriller", False)]
 
     def build(self):
         self.title = "Movies To Watch 2.0 - by Jonache Hilton"
