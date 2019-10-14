0b8981421116380d037357d7c565ab776b1ad65e
Jonache Hilton <jonache.hilton@my.jcu.edu.au>
2019-10-14 19:32:34 +1000

    Create the build method for the MoviesToWatchApp and create a box layout

--- main.py
+++ main.py
@@ -4,14 +4,22 @@ Date:
 Brief Project Description:
 GitHub URL:
 """
-# TODO: Create your main program in this file, using the MoviesToWatchApp class
 
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
+from movie import Movie
+from moviecollection import MovieCollection
 
 
 class MoviesToWatchApp(App):
     """..."""
-    pass
+
+    def build(self):
+        self.title = "Movies To Watch 2.0 - by Jonache Hilton"
+        self.root = Builder.load_file('app.kv')
+        return self.root
 
 
 if __name__ == '__main__':
