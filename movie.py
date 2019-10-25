8ee5a67b0477f51b31ec28ac196f6306236718b7
Jonache Hilton <jonache.hilton@my.jcu.edu.au>
2019-10-25 14:30:40 +1000

    Update docstrings for Movie class

--- movie.py
+++ movie.py
@@ -1,7 +1,13 @@
-"""..."""
+"""
+Name: Jonache Hilton
+Date: 25/10/2019
+Movie Class
+GitHub URL: https://github.com/cp1404-students/2019-2-a2-jonachehilton
+"""
 
 
 class Movie:
+    """"Represent a Movie object."""
     def __init__(self, title="", year=0, category="", is_watched=False):
         self.title = title
         self.year = int(year)
@@ -9,6 +15,7 @@ class Movie:
         self.is_watched = is_watched
 
     def __str__(self):
+        """Return a string representation of a Movie object."""
         if self.is_watched:
             watched_string = "watched"
         else:
@@ -16,7 +23,9 @@ class Movie:
         return "{} ({} from {}) {}".format(self.title, self.category, self.year, watched_string)
 
     def watch(self):
+        """Change the watched status to True for a Movie object"""
         self.is_watched = True
 
     def unwatch(self):
+        """Change the watched status to False for a Movie object"""
         self.is_watched = False
